6f791cf7d94753b5bba370bb50719e19c8cba224
Joshua Mackelprang <joshua@mackelprang.com>
2026-03-25 12:08:24 -0400

    fix: revert Url/url to string type for downstream compatibility, suppress CA1054/CA1056

--- src/Iaet.Core/Models/CapturedRequest.cs
+++ src/Iaet.Core/Models/CapturedRequest.cs
@@ -6,7 +6,8 @@ public sealed record CapturedRequest
     public required Guid SessionId { get; init; }
     public required DateTimeOffset Timestamp { get; init; }
     public required string HttpMethod { get; init; }
-    public required Uri Url { get; init; }
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1056:URI-like properties should not be strings")]
+    public required string Url { get; init; }
     public required Dictionary<string, string> RequestHeaders { get; init; }
     public string? RequestBody { get; init; }
     public required int ResponseStatus { get; init; }
